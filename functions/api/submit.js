810422fba5bab3ecce61a4a8c46ec45badfe3de9
Martin Groeneveld <3328493+groveld@users.noreply.github.com>
2024-04-29 14:41:01 +0200

    test

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -6,7 +6,7 @@ export async function onRequestPost(context) {
   }
 }
 
-async function handleRequest({ request, env }) {
+async function handleRequest({ request }) {
   let formData = await request.formData();
   let name = formData.get('name');
   let email = formData.get('email');
@@ -18,8 +18,7 @@ async function handleRequest({ request, env }) {
     return new Response('Missing required fields', { status: 400 });
   }
 
-  let turnstileSecret = env.TURNSTILE_SECRET_KEY;
-  let captchaValidated = await verifyCaptcha(turnstileSecret, token, ip);
+  let captchaValidated = await verifyCaptcha(token, ip);
 
   if (!captchaValidated) {
     return new Response('Invalid captcha', { status: 403 });
@@ -30,9 +29,9 @@ async function handleRequest({ request, env }) {
   return new Response('Message sent', { status: 200 });
 }
 
-async function verifyCaptcha(secret, token, ip) {
+async function verifyCaptcha(token, ip, { env }) {
   let formData = new FormData();
-  formData.append("secret", secret);
+  formData.append("secret", env.TURNSTILE_SECRET_KEY);
   formData.append("response", token);
   formData.append("remoteip", ip);
 
@@ -46,15 +45,14 @@ async function verifyCaptcha(secret, token, ip) {
   return outcome.success;
 }
 
-async function sendEmailWithMailgun({ name, email, message, env }) {
-  let mailgunApiUrl = env.MAILGUN_API_URL;
+async function sendEmailWithMailgun(name, email, message, { env }) {
   let mailgunDomain = env.MAILGUN_DOMAIN;
   let mailgunApiKey = env.MAILGUN_API_KEY;
   let mailgunFromName = env.MAILGUN_FROM_NAME;
   let mailgunFromAddress = env.MAILGUN_FROM_ADDRESS;
   let mailgunToName = env.MAILGUN_TO_NAME;
   let mailgunToAddress = env.MAILGUN_TO_ADDRESS;
-  let mailgunUrl = `https://${mailgunApiUrl}/v3/${mailgunDomain}/messages`;
+  let mailgunUrl = `https://api.mailgun.net/v3/${mailgunDomain}/messages`;
 
   let formData = new FormData();
   formData.append("from", `${mailgunFromName} <${mailgunFromAddress}>`);
