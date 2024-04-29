d9f1df195d26996d88fd5551a4b41eb887c2b765
Martin Groeneveld <3328493+groveld@users.noreply.github.com>
2024-04-29 13:14:21 +0200

    test contact form with captcha

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -1,29 +1,52 @@
-/**
- * POST /api/submit
- */
+// POST /api/submit
+
 export async function onRequestPost(context) {
-    try {
-      let input = await context.request.formData();
-
-      // Convert FormData to JSON
-      // NOTE: Allows multiple values per key
-      let output = {};
-      for (let [key, value] of input) {
-        let tmp = output[key];
-        if (tmp === undefined) {
-          output[key] = value;
-        } else {
-          output[key] = [].concat(tmp, value);
-        }
-      }
-
-      let pretty = JSON.stringify(output, null, 2);
-      return new Response(pretty, {
-        headers: {
-          'Content-Type': 'application/json;charset=utf-8',
-        },
-      });
-    } catch (err) {
-      return new Response('Error parsing JSON content', { status: 400 });
-    }
+  try {
+    return await handleRequest(context);
+  } catch (err) {
+    return new Response('Error sending message', { status: 500 });
   }
+}
+
+async function handleRequest(request) {
+  let formData = await request.formData();
+  let name = formData.get('name');
+  let email = formData.get('email');
+  let message = formData.get('message');
+  let token = formData.get('cf-turnstile-response');
+  let ip = request.headers.get('cf-connecting-ip');
+
+  if (!name || !email || !message) {
+    return new Response('Missing required fields', { status: 400 });
+  }
+
+  let captchaValidated = await verifyCaptcha(token, ip);
+
+  if (!captchaValidated) {
+    return new Response('Invalid captcha', { status: 403 });
+  }
+
+  // await sendEmailWithMailgun({ name, email, message });
+
+  return new Response('Message sent', { status: 200 });
+}
+
+async function verifyCaptcha(token, ip) {
+  let secret = context.env.TURNSTILE_SECRET_KEY;
+  let formData = new FormData();
+  formData.append("secret", secret);
+  formData.append("response", token);
+  formData.append("remoteip", ip);
+
+  let url = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
+  let result = await fetch(url, {
+    body: formData,
+    method: "POST",
+  });
+
+  let outcome = await result.json();
+  return outcome.success;
+}
+
+// async function sendEmailWithMailgun(message) {
+// }
