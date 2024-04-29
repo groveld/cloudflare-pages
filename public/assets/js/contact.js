6a949662609227086740f37c8513fbbc99374033
Martin Groeneveld <3328493+groveld@users.noreply.github.com>
2024-04-29 22:29:57 +0200

    Refactor form submission logic and update button text in contact.js

--- public/assets/js/contact.js
+++ public/assets/js/contact.js
@@ -1,6 +1,9 @@
 // Get the form element with the id "contact-form"
 const contactForm = document.getElementById("contact-form");
 
+// Get the submit button
+const submitButton = document.getElementById("contact-form-submit");
+
 // Add an event listener to the form element
 contactForm.addEventListener("submit", submitForm);
 
@@ -8,16 +11,10 @@ contactForm.addEventListener("submit", submitForm);
 function submitForm(event) {
 event.preventDefault();
 
-// document
-//     .querySelectorAll("input, textarea, button")
-//     .forEach((input) => (input.disabled = true));
-
-// Get the submit button
-const submitButton = document.getElementById("contact-form-submit");
-
 // Change the button text to "Sending message..."
 submitButton.textContent = "Sending message...";
 
+// Get the form data
 const formData = new FormData(event.target);
 
 // Send the form to the Cloudflare Workers API
@@ -26,31 +23,34 @@ fetch("/api/contact", {
     body: formData,
 })
     .then((response) => {
-    // Parse the response text and include the response status
-    return response.text().then((text) => ({
-        text,
-        ok: response.ok,
-    }));
+        if (response.ok) {
+            // Change the button text to "Message sent!"
+            submitButton.textContent = "Message sent!";
+            // Reset the form
+            contactForm.removeEventListener("submit", submitForm);
+            contactForm.reset();
+        } else {
+            // Change the button text to "Error sending message"
+            submitButton.textContent = "Error sending message";
+
+            // Reset the button text after 5 seconds
+            setTimeout(() => {
+                submitButton.textContent = "Send message";
+            }, 5000);
+        }
     })
-    .then(({ text, ok }) => {
-    // If the request was successful, disable the form
-    if (ok) {
-        // You can modify/add something else here
-        contactForm.removeEventListener("submit", submitForm);
-        contactForm.reset();
-        // Change the button text to "Message sent!"
-        submitButton.textContent = "Message sent!";
-        submitButton.classList.add("btn-success");
-    } else {
-        // If the request was not successful, throw an error
-        submitButton.textContent = "Something went wrong!";
-        submitButton.classList.add("btn-danger");
-        throw new Error();
-    }
-    })
-    // Catch any errors that occur during the fetch request
-    .catch(() => {
-    console.log("Error");
-    // You can add something that should happen when the form failed
+    .catch((error) => {
+        // Change the button text to "Error sending message"
+        submitButton.textContent = "Error sending message";
+
+        // Reset the button text after 5 seconds
+        setTimeout(() => {
+            submitButton.textContent = "Send message";
+        }, 5000);
     });
 }
+
+
+// document
+//     .querySelectorAll("input, textarea, button")
+//     .forEach((input) => (input.disabled = true));
